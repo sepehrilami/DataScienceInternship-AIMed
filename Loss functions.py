c1b1314a5307c6e315284227f003b6995e53390f
sepehrilami <sepehrilami@yahoo.com>
2020-08-18 08:25:36 +0430

    softmax and cross-entropy updated

--- Loss functions.py
+++ Loss functions.py
@@ -118,3 +118,38 @@ def showcase_loss(guess, true_price, risk=50000):
     else:
         return np.abs(true_price - guess - dif)
 
+
+def softmax(X):
+    exps = np.exp(X)
+    return exps / np.sum(exps)
+
+
+def cross_entropy_2(X,y):
+    """
+    X is the output from fully connected layer (num_examples x num_classes)
+    y is labels (num_examples x 1)
+    	Note that y is not one-hot encoded vector.
+    	It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
+    """
+    m = y.shape[0]
+    p = softmax(X)
+    # We use multidimensional array indexing to extract
+    # softmax probability of the correct label for each sample.
+    # Refer to https://docs.scipy.org/doc/numpy/user/basics.indexing.html#indexing-multi-dimensional-arrays for understanding multidimensional array indexing.
+    log_likelihood = -np.log(p[range(m),y])
+    loss = np.sum(log_likelihood) / m
+    return loss
+
+
+def delta_cross_entropy(X,y):
+    """
+    X is the output from fully connected layer (num_examples x num_classes)
+    y is labels (num_examples x 1)
+    	Note that y is not one-hot encoded vector.
+    	It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
+    """
+    m = y.shape[0]
+    grad = softmax(X)
+    grad[range(m),y] -= 1
+    grad = grad/m
+    return grad
